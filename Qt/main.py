610004d1a94503c298c277800040482a635e4ab4
Jeff Hoogland <jeffhoogland@linux.com>
2015-09-17 21:18:22 -0500

    Started work on a Qt port so this can be cross platform

---
+++ Qt/main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide.QtGui import *
+from PySide.QtCore import *
+from ui_mainWindow import Ui_mainWindow
+
+class MainWindow(QMainWindow, Ui_mainWindow):
+   def __init__(self):
+       super(MainWindow, self).__init__()
+       self.setupUi(self)
+       self.assignWidgets()
+       self.show()
+   
+   def assignWidgets(self):
+       pass
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   mainWin = MainWindow()
+   ret = app.exec_()
+   sys.exit( ret )
